7227d56b5598f18b7d725cd3bc1e23e24b2790fe
omarjaab <omarjaab@outlook.com>
2016-05-27 09:07:15 +0200

    testgithub

---
+++ first/Controllers/testinggggithubController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace first.Controllers
+{
+    public class testinggggithubController : Controller
+    {
+        // GET: testinggggithub
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
